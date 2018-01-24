daf01fd00098313a714c2f186b9cb996960b3e12
Tanvir <tanvir0102@gmail.com>
2018-01-24 19:37:51 +0530

    create a aws instance using AMIid and output the instance id

---
+++ create_instance.py
@@ -0,0 +1,9 @@
+#!/bin/python
+import boto3
+ec2 = boto3.resource('ec2')
+instance = ec2.create_instances(
+	ImageId='ami-97785bed',
+	MinCount=1,
+	MaxCount=1,
+	InstanceType='t2.micro')
+print instance[0].id
