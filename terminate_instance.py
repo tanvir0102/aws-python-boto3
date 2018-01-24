97d56ae4b52725620ce85a87d816e0a86c5754e4
Tanvir <tanvir0102@gmail.com>
2018-01-24 20:01:33 +0530

    pyhton script to terminate using instance.id

---
+++ terminate_instance.py
@@ -0,0 +1,8 @@
+#!/bin/python
+import sys
+import boto3
+ec2 = boto3.resource('ec2')
+for instance_id in sys.argv[1:]:
+	instance = ec2.Instance(instance_id)
+	response = instance.terminate()
+	print response
